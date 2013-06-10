124a6483fca5a523bebcefa2cc79755a5ec46a99
Garrett Dawson <killtheliterate@gmail.com>
2013-06-10 12:49:31 -0600

    Add block component examples

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 (function ($) {
   $(document).ready(function(){
 
+    // Toggle dropdown class.
+    // -----------------------------------------------------------------------
+    $('.block--dropdown').find('.block-title').click(function() {
+      $(this).parents('.block--dropdown').toggleClass('is-expanded');
+    });
+
   });
 })(jQuery);
--- template.php
+++ template.php
@@ -9,3 +9,17 @@ include 'inc/field.inc';
 include 'inc/form.inc';
 include 'inc/menu.inc';
 include 'inc/node.inc';
+
+function prototype_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+
+  if (!empty($breadcrumb)) {
+
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+    $output .= '<div class="breadcrumb">' . implode(' <span class="breadcrumb-separator">/</span> ', $breadcrumb) . '</div>';
+    return $output;
+  }
+}
