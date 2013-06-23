4425b63fb06e44042cff42ce837938fd45bcfae6
Garrett Dawson <killtheliterate@gmail.com>
2013-06-23 16:20:17 -0400

    Add styleguide php file.

---
+++ styleguide/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="gt-ie8" lang="en"> <!--<![endif]-->
+<head>
+  <title></title>
+  <meta charset="utf-8" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge, chrome=1" />
+  <meta name="MobileOptimized" content="width" />
+  <meta name="HandheldFriendly" content="true" />
+  <meta name="viewport" content="width=device-width" />
+  <meta http-equiv="cleartype" content="on" />
+</head>
+
+<body>
+  <div>
+  </div>
+</body>
+</html>
